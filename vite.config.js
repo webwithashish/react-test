79c1ad67c786f2685c12ff9f862582eed724a706
Ashish Dattatraya Latake <ashishlatake412@gmail.com>
2024-01-24 11:03:37 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/react-basic/"
-  plugins: [react()]
+    base: "/react-basic/",
+    plugins: [react()],
 });
